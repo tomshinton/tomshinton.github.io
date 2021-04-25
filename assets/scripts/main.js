ca6beef2004b250ed743ce458ed026d3841bdda4
Tom <tomshinton@googlemail.com>
2021-04-25 01:28:00 +0100

    Refactored stylesheets and added social panels

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,54 +1,62 @@
-//Load all games from the publishedGames.xml and rebuild the main page
-
 let publishedGamesUrl = "assets/data/xml/publishedGames.xml";
+let blogListUrl= "assets/content/blog/TestBlog.html";
 
-function BuildHomePage(data)
-{ 
-    var mainBody = document.getElementById("main");
-
-    let games = data.getElementsByTagName('game');
-    console.log("Found " + games.length + " games in Published Games xml");
-
-    if(games.length > 0)
+function FindBlogPosts() 
+{
+    fetch(blogListUrl).then(response => response.text()).then(data =>
     {
-        console.log("Building homepage");
-        
-        for(let i=0; i<games.length; ++i)
-        {
-            //Create new Card
-            let newCard = document.createElement("div");
-            newCard.className = "card";
-    
-            //Create Image
-            let bannerImage = document.createElement("img");
-            bannerImage.src = games[i].getElementsByTagName('bannerUrl')[0].innerHTML;
-            bannerImage.className = "gameBanner"
-            newCard.appendChild(bannerImage);
+        let Parser = new DOMParser();
+        let html = Parser.parseFromString(data, "text/html");
 
-            //Create inner text and fading section
-            let bannerMiddle = document.createElement("div");
-            bannerMiddle.className = "gameBannerMiddle";
-            
-            let bannerText = document.createElement("div");
-            bannerText.className = "gameBannerText";
-
-            bannerText.textContent = games[i].getElementsByTagName('name')[0].innerHTML;
-
-            bannerMiddle.appendChild(bannerText);
-
-            newCard.appendChild(bannerMiddle);
-            mainBody.appendChild(newCard);
-        }        
-    }
+        console.log(html.getElementsByTagName('meta')[0].content);
+    });
 }
 
-fetch(publishedGamesUrl)    
-.then(response=>response.text())
-.then(data=>
+function BuildHomePage() 
 {
-    let Parser = new DOMParser();
-    let xml = Parser.parseFromString(data, "application/xml");
+    fetch(publishedGamesUrl).then(response => response.text()).then(data =>
+    {
+        let Parser = new DOMParser();
+        let xml = Parser.parseFromString(data, "application/xml");
 
-    BuildHomePage(xml);
-});
+        var mainBody = document.getElementById("main");
 
+        let games = xml.getElementsByTagName('game');
+        console.log("Found " + games.length + " games in Published Games xml");
+
+        if (games.length > 0) 
+        {
+            console.log("Building homepage");
+
+            for (let i = 0; i < games.length; ++i) 
+            {
+                //Create new Card
+                let newCard = document.createElement("div");
+                newCard.className = "card";
+
+                //Create Image
+                let bannerImage = document.createElement("img");
+                bannerImage.src = games[i].getElementsByTagName('bannerUrl')[0].innerHTML;
+                bannerImage.className = "gameBanner"
+                newCard.appendChild(bannerImage);
+
+                //Create inner text and fading section
+                let bannerMiddle = document.createElement("div");
+                bannerMiddle.className = "gameBannerMiddle";
+
+                let bannerText = document.createElement("div");
+                bannerText.className = "gameBannerText";
+
+                bannerText.textContent = games[i].getElementsByTagName('name')[0].innerHTML;
+
+                bannerMiddle.appendChild(bannerText);
+
+                newCard.appendChild(bannerMiddle);
+                mainBody.appendChild(newCard);
+            }
+        }
+    });
+}
+
+BuildHomePage();
+FindBlogPosts();
